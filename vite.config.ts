d8623967ef0baa7b251e78967e0fb6e528457637
kundalaryan <aryan4321.ak@gmail.com>
2025-08-30 09:36:53 +0530

    Setup GitHub Actions for Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "portfolio", // 👈 IMPORTANT for GitHub Pages
+  base: "/codequintet-nexus/", // 👈 IMPORTANT for GitHub Pages
   server: {
     host: "::",
     port: 8080,
